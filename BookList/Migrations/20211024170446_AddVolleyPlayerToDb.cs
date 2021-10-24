cd0a5ded811df832292b6cffbf337114e894acff
BlerionMuco <66955735+BlerionMuco@users.noreply.github.com>
2021-10-24 19:10:15 +0200

    Connection with Db

---
+++ BookList/Migrations/20211024170446_AddVolleyPlayerToDb.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BookList.Migrations
+{
+    public partial class AddVolleyPlayerToDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "VolleyPlayer",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: false),
+                    LastName = table.Column<string>(nullable: false),
+                    Rate = table.Column<double>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_VolleyPlayer", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "VolleyPlayer");
+        }
+    }
+}
